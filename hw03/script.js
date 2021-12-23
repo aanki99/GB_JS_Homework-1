b9e63cd80d73eb21a8fc5bdce2f073489fff769f
Alexander Selyuk <alexanderselyuk@gmail.com>
2021-12-23 18:01:45 +0300

    выполнел задание 1

--- hw03/script.js
+++ hw03/script.js
@@ -52,7 +52,6 @@ return sum;
 console.log('!Ответ! Стоимость корзины: ' + countCartPrice(basket));
 
 
-
 console.log('Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так: for(…){// здесь пусто');
 
 for (let i = 0; i < 10; console.log(i++)) {}
